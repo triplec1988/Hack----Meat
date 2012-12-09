71e8d46efdb36d2bc8f4a036ffa0fa92c21c012e
Chris Clouten <triplec1988@gmail.com>
2012-12-08 22:44:58 -0500

    Added Unicode to the models

--- hackmeat/reservation/models.py
+++ hackmeat/reservation/models.py
@@ -19,12 +19,18 @@ class Reservation(models.Model):
     pickup_time = models.DateTimeField()
     status = models.CharField(max_length=2, choices=STATUS)
 
+    def __unicode__(self):
+        return u'{0} - {3}'.format(self.farmer.farm_name, self.processor.plant_name, self.dropoff_time, self.status, )
+
 
 class Blackout(models.Model):
     processor = models.ForeignKey(Processor, related_name='closed')
     start_time = models.DateField()
     end_time = models.DateField()
     reason = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return u'{0} - {2}'.format(self.processor, self.start_time, self.end_time, )
 
 
 class Animal_Reservation(models.Model):
@@ -33,6 +39,12 @@ class Animal_Reservation(models.Model):
     animal_quantity = models.PositiveIntegerField()
     cut = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return u'{0} - {2}'.format(self.reservation, self.animal, self.animal_quantity, )
+
 
 class Animal(models.Model):
     name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name, )
