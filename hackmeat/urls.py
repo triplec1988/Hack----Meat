a8add4ca86f8e30025ed1b87c585ca236c71435a
Ethan Lo <arkitex@gmail.com>
2012-12-09 00:26:00 -0500

    * processor_reserve page with calendar* processor_reserve page with calendar* processor_reserve page with calendar* processor_reserve page with calendar* processor_reserve page with calendar* processor_reserve page with calendar* processor_reserve page with calendar* processor_reserve page with calendar

--- hackmeat/urls.py
+++ hackmeat/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('hackmeat.base.urls')),
     url(r'^u/', include('hackmeat.account.urls')),
+    url(r'^r/', include('hackmeat.reservation.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
