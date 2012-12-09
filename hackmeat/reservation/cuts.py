2a0993345bd756a47d11c4741f68affe1d445de4
Chris Clouten <triplec1988@gmail.com>
2012-12-09 02:38:05 -0500

    updated forms.py and cuts.py

--- hackmeat/reservation/cuts.py
+++ hackmeat/reservation/cuts.py
@@ -1,1 +1,75 @@
-PORK_SHOULDER
+PORK_SHOULDER = (
+    ('BB', 'Boston Butt Roast'),
+    ('PR', 'Picnic Roast'),
+    ('LR', '1 Large Roast'),
+    ('SR', '2 Small Roasts'),
+)
+
+PORK_LOIN = (
+    ('WR', 'Whole Rack Baby Back Ribs'),
+    ('WT', 'Whole Tenderloin'),
+    ('BT', 'Whole Boneless Tenderloin'),
+    ('BCU', 'Boneless Cutlets'),
+    ('BCH', 'Boneless Chops'),
+    ('BIC', 'Bone-In Chops'),
+)
+
+PORK_BELLY = (
+    ('SP', 'Whole Rack Spare Ribs'),
+    ('PB', 'Pork Belly - Uncured'),
+    ('SB', 'Smoked Bacon'),
+)
+
+PORK_LEG = (
+    ('LR', 'Very Large Leg Roast'),
+    ('LH', '2 Leg Roast and 1 Hock'),
+    ('LS', 'Leg Steaks'),
+    ('PG', 'All Ground'),
+)
+
+PORK_SAUSAGE = (
+    ('BS', 'Breakfast Sausage'),
+    ('HIS', 'Hot Italian Sausage'),
+    ('SIS', 'Sweet Italian Sausage'),
+    ('CH', 'Chorizo')
+)
+
+PORK_OTHER = (
+    ('PHD', 'Pork Head'),
+    ('PGT', 'Pig Tail'),
+    ('PGE', 'Pig Ears'),
+    ('PTR', 'Trotters'),
+)
+
+BEEF_RIB = (
+    ('BI', 'Bone-In Ribeye'),
+    ('BL', 'Boneless Ribeye'),
+)
+
+BEEF_LOIN = (
+    ('FM', 'Filet Mignon'),
+    ('NY', 'New York Strip'),
+    ('TB', 'T-Bone'),
+    ('PT', 'Porterhouse')
+)
+
+BEEF_SIRLOIN = (
+    ('TS', 'Top Sirloin'),
+    ('OS', 'Other Sirloin'),
+)
+
+BEEF_ROUND = (
+    ('ER', 'Eye of Round Roast'),
+    ('TRR', 'Top Round Roast'),
+    ('BRR', 'Bottom Round Roast'),
+    ('LB', 'London Broil')
+)
+
+BEEF_OTHER = (
+    ('WBR', 'Whole Brisket'),
+    ('FS', 'Flank Steak'),
+    ('SM', 'Stew Meat'),
+    ('BFL', 'Sliced Liver'),
+    ('BFH', 'Heart'),
+    ('BFT', 'Tongue'),
+)
--- hackmeat/reservation/forms.py
+++ hackmeat/reservation/forms.py
@@ -1,3 +1,23 @@
 from django import forms
-from django.core validators import *
+from django.core.validators import *
 
+ANIMALS = (
+    ('P', 'Pigs'),
+    ('C', 'Cattle'),
+)
+
+
+class ReservationForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    farm_name = forms.CharField(max_length=100)
+    animal_type_one = forms.ChoiceField(choices=ANIMALS)
+    animal_quantity_one = forms.IntegerField()
+    animal_type_two = forms.ChoiceField(choices=ANIMALS, required=False)
+    animal_quantity_two = forms.IntegerField(required=False)
+    dropoff = forms.DateTimeField()
+    pickup = forms.DateTimeField()
+
+
+class CutForm(forms.Form):
+	pork_shoulder
