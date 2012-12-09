6d7ea7ba8509d37cb1655abeb389afb68fb6f94c
Matthew Conlen <mc@mathisonian.com>
2012-12-08 22:10:01 -0500

    pushing urls and templates

---
+++ hackmeat/account/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('hackmeat.account.views',
+    url(r'^processors/?$', r'processors'),
+    url(r'^signup/farmer?$', r'signup_farmer'),
+    url(r'^signup/processor?$', r'signup_processor'),
+    url(r'^processor/((?P<processor>\d+))/?$', r'processor'),
+)
--- hackmeat/account/views.py
+++ hackmeat/account/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+
+def processors(request):
+    return render_to_response('account/processors.html',
+                            {},
+                            context_instance=RequestContext(request))
+
+
+def signup_farmer(request):
+    return render_to_response('account/signup_farmer.html',
+                            {},
+                            context_instance=RequestContext(request))
+
+
+def signup_processor(request):
+    return render_to_response('account/signup_processor.html',
+                            {},
+                            context_instance=RequestContext(request))
+
+
+def processor(request):
+    return render_to_response('account/processor.html',
+                            {},
+                            context_instance=RequestContext(request))
