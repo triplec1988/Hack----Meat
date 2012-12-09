0375839e9969bafa127d1d27064b56a513cac2ea
Matthew Conlen <mc@mathisonian.com>
2012-12-08 22:22:18 -0500

    account url fix

--- hackmeat/account/urls.py
+++ hackmeat/account/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('hackmeat.account.views',
-    url(r'^processors/?$', r'processors'),
-    url(r'^signup/farmer?$', r'signup_farmer'),
-    url(r'^signup/processor?$', r'signup_processor'),
-    url(r'^processor/((?P<processor>\d+))/?$', r'processor'),
+    url(r'processors/?$', r'processors'),
+    url(r'signup/farmer/?$', r'signup_farmer'),
+    url(r'signup/processor/?$', r'signup_processor'),
+    url(r'processor/((?P<processor>\d+))/?$', r'processor'),
 )
