16175855c786b436587e77bc9b2ab2de94cfe803
Matthew Conlen <mc@mathisonian.com>
2012-12-08 23:07:57 -0500

    admin fix

--- hackmeat/urls.py
+++ hackmeat/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,12 +7,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('hackmeat.base.urls')),
-    url(r'^u/', include('hackmeat.account.urls')),    
+    url(r'^u/', include('hackmeat.account.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.LOCAL:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
