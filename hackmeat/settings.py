aba6a66ab0434b3085a556ce3f9e1e17616f47a7
Chris Clouten <triplec1988@gmail.com>
2013-01-24 00:55:17 -0500

    added default database

--- hackmeat/settings.py
+++ hackmeat/settings.py
@@ -20,14 +20,14 @@ MANAGERS = ADMINS
 DATABASES = {
 # Uncomment for local development
 #    'local': {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'hackmeat.db',
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'hackmeat.db',
 #        'USER': '',
 #        'PASSWORD': '',
 #        'HOST': '',
 #        'PORT': '',
-   }
+#   }
 }
 
 if LOCAL:
